c84bd26a23de09752dd051b4ac1fa3c6085128c7
NYXBAM <NYXBAM@proton.me>
2025-08-04 23:30:52 -0700

    tuples

--- chapter2/tuples.py
+++ chapter2/tuples.py
@@ -14,3 +14,40 @@ for passport in sorted(traveler_ids):
 
 for country, _ in traveler_ids:
     print(country)  # Output: USA, BRA, ESP
+
+
+# Tuple unpacking 
+lax_coordinates = (33.9425, -118.408056)  # Tuple representing latitude and longitude
+latitude, longitude = lax_coordinates  # Unpacking the tuple
+print(latitude)  # Output: 33.9425
+print(longitude)  # Output: -118.408056 
+
+
+import os
+
+_, filename = os.path.split('/home/username/documents/file.txt')  # Unpacking the result of os.path.split
+print(filename)  # Output: file.txt
+
+
+a, b, *rest = range(5)
+print(a)
+print(b)
+print(rest)
+
+
+######################################
+
+metro_areas = [
+    ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', 'IN', 26.072, (28.613889, 77.209444)),
+    ('Mexico City', 'MX', 20.142, (19.432778, -99.133333)),
+    ('New York-Newark', 'US', 20.104, (40.712778, -74.006111)),
+    ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
+
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:^9.4f} | {:^9.4f}'
+
+for name, cc, pop, (latitude, longitude) in metro_areas:
+    if longitude <= 0:
+        print(fmt.format(name, latitude, longitude))
