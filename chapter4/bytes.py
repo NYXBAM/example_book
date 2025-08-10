b4b286970da31e1d0eb4c19260256b398394d105
NYXBAM <NYXBAM@proton.me>
2025-08-10 02:24:51 -0700

    chapter4

--- chapter4/bytes.py
+++ chapter4/bytes.py
@@ -81,8 +81,34 @@ def shave_marks(txt):
 
 
 order = '<>><><>><Hello ĉ, ĝ, ĥ, ĵ,sdkas Hersssr Voß: # § ⸹ ¶ ⸿ (capitulum) № ⌘ ჻ ፨ ※ ℹ ⅈ '
-
-
-
 shaved_order = shave_marks(order)
 print(shaved_order) 
+
+
+#### --- Sorting UNICODE --- ####
+
+fruits = ['kiwi', 'apple', 'orange', 'banana', 'grape', 'pear', 'caju', 'caja']
+
+print(sorted(fruits))  # Default sort
+
+
+import unicodedata
+import re 
+
+re_digit = re.compile(r'\d')
+
+sample = "1\xbc\xb2\u0969\u136b\u216b\u2466\u2480\u3285"
+
+for char in sample:
+    print('U+%04x' % ord(char),
+    char.center(6),
+    're_dig' if re_digit.match(char) else '-',
+    'isdig' if char.isdigit() else '-',
+    'isnum' if char.isnumeric() else '-',
+    format(unicodedata.numeric(char), '5.2f'),
+    unicodedata.name(char),
+    sep='\t')
+
+import os 
+
+os.listdir('.')
