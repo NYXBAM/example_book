ad1d67c50850579d24de0b5840db9fbcab17d4a6
NYXBAM <NYXBAM@proton.me>
2025-08-15 04:10:46 -0700

    func

--- chapter5/func.py
+++ chapter5/func.py
@@ -120,3 +120,32 @@ for city in sorted(metro_data, key=itemgetter(3)):
 cc_name = itemgetter(1, 0)
 for city in metro_data:
     print(cc_name(city))  # ('JPN', 'Tokyo'), ('IND', 'Delhi'), ...
+
+
+
+# ----------------------------------------------------- #
+
+from collections import namedtuple
+LatLong = namedtuple('LatLong', 'lat long')
+Metropolis = namedtuple('Metropolis', 'name cc pop coord')
+metro_areas = [Metropolis(name, cc, pop, LatLong(lat, long))
+               for name, cc, pop, (lat, long) in metro_data]
+print(metro_areas[0])
+print(metro_areas[0].coord.lat)  # 36.933
+
+from operator import attrgetter
+
+name_lat = attrgetter('name', 'coord.lat')
+for city in sorted(metro_areas, key=attrgetter('coord.lat')):
+    print(name_lat(city))  # ('Tokyo', 35.689722), ('Delhi', 28.613889), ...
+
+
+# ----------------------------------------------------------- #
+from operator import methodcaller
+
+s = 'The time has come'
+upcase = methodcaller('upper')
+print(upcase(s))  # THE TIME HAS COME
+print(str.upper(s))  # THE TIME HAS COME
+hiphenate = methodcaller('replace', ' ', '-')
+print(hiphenate(s))  # The-time-has-come
