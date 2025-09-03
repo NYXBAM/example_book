5de9f9d684edd3379ffe59db4a4ab0e4ec854e6c
NYXBAM <NYXBAM@proton.me>
2025-09-03 00:35:39 -0700

    finish chapter 7

--- chapter8/reference.py
+++ chapter8/reference.py
@@ -201,4 +201,24 @@ result1 = wref() is None  # False
 print(wref() is None) # False
 
 # --------------- WeakValueDictionary --------------- #
-# print('\nWeakValueDictionary:')
+print('\nWeakValueDictionary:')
+
+class Cheese:
+    def __init__(self, kind):
+        self.kind = kind
+    def __repr__(self):
+        return 'Cheese(%r)' % self.kind
+    
+stock = weakref.WeakValueDictionary()
+catalog = [Cheese('Red Leicester'), Cheese('Tilsit'), Cheese('Brie'), Cheese('Parmesan')]
+for cheese in catalog:
+    stock[cheese.kind] = cheese
+print(sorted(stock.keys())) # ['Brie', 'Parmesan', 'Red Leicester', 'Tilsit']
+del catalog
+print(sorted(stock.keys())) # ['Parmesan']
+del cheese
+print(sorted(stock.keys())) # []
+
+
+
+
