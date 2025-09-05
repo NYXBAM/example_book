1700607b7a13213cc021dedee6ec6c71b8911d7c
NYXBAM <NYXBAM@proton.me>
2025-09-05 02:08:58 -0700

    objects, formats, static and classmethod

--- chapter9/obj.py
+++ chapter9/obj.py
@@ -33,6 +33,12 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self)) 
     
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+    
     
 v1 = Vector2d(3, 4)
 print(v1.x, v1.y) # 3.0 4.0
@@ -46,3 +52,31 @@ octets = bytes(v1)
 print(octets) # b'd\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x10@'
 print(abs(v1)) # 5.0 # 
 print(bool(v1), bool(Vector2d(0, 0))) # True False
+
+# --------------------- classmethod --------------------- # 
+print("*"*40)
+print('\n\n@classmethod\n')
+
+
+class Demo:
+    @classmethod
+    def klassmeth(*args):
+        return args
+    @staticmethod
+    def statmeth(*args):
+        return args
+    
+    
+print(Demo.klassmeth()) # (<class '__main__.Demo'>,)
+print(Demo.klassmeth('spam')) # (<class '__main__.Demo'>, 'spam')
+print(Demo.statmeth()) # ()
+print(Demo.statmeth('spam')) # ('spam',)
+
+# --------------------- format --------------------- # 
+print("*"*40)
+print('\noutput format \n')
+
+brl = 1/2.43 #
+
+print(format(brl, '0.4f')) # 0.4115
+print('1 BRT = {rate:0.2f} USD'.format(rate=brl))
