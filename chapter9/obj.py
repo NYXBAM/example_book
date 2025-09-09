23d259033f2aec98e20b0087075307189b64e7cc
NYXBAM <NYXBAM@proton.me>
2025-09-09 02:23:38 -0700

    finish chapter 9

--- chapter9/obj.py
+++ chapter9/obj.py
@@ -214,7 +214,99 @@ v1 = Vector2d(3, 4)
 print(v1.__dict__) # {'_Vector2d__x': 3.0, '_Vector2d__y': 4.0}
 
 # ---------------------- __slots__ ---------------------- # 
-# print("*"*40)
-# print('\n__slots__ \n')
+print("*"*40)
+print('\n__slots__ \n')
 # u can see how i used slots in practice here: 
 # https://github.com/NYXBAM/CryptoWhaleMonitor/blob/master/models/dataclass.py
+
+class Vector2d:
+    __slots__ = ('__x', '__y')
+    typecode = 'd'
+    # other methods are the same as above
+    
+    
+# if u need to add some dynamic attribute use this:
+class Vector2d:
+    __slots__ = ('__x', '__y', '__dict__', '__weakref__') # __weakref__ and  __dict__ 
+    typecode = 'd'
+    # other methods are the same as above
+
+
+class Vector2d:
+    typecode = 'd'
+    
+    def __init__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+    @property    
+    def x(self):
+        return self.__x
+    @property
+    def y(self):
+        return self.__y
+    
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+    
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+    
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+    
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, *self)
+    
+    def __str__(self):
+        return str(tuple(self))
+    
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + 
+                bytes(array(self.typecode, self)))
+        
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+    
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+    def __bool__(self):
+        return bool(abs(self)) 
+    
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+    
+    def angle(self):
+        return math.atan2(self.y, self.x)
+    
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(*memv)
+   
+   
+v1 = Vector2d(1.1, 2.2)
+dumpd = bytes(v1)
+print(dumpd)  # b'd\x9a\x99\x99\x99\x99\x99\xf1?\x9a\x99\x99\x99\x99\x99\x01@'
+v1.typecode = 'f' # change typecode for this instance only
+dumpf = bytes(v1)
+print(dumpf) # b'f\xcd\xcc\x8c?\xcd\xcc\x0c@'
+print(len(dumpd), len(dumpf)) # 17 9
+print(Vector2d.typecode) # d
+
+class ShortVector2d(Vector2d):
+    typecode = 'f'
+    
+sv = ShortVector2d(1/11, 1/27)
+print(sv) # (0.09090909090909091, 0.037037037037037035)
+print(len(bytes(sv))) # 9 
