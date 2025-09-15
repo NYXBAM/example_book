eeaa86bd6a7fef06dd903af4e5a42b87edfdebd4
NYXBAM <NYXBAM@proton.me>
2025-09-15 08:24:34 -0700

    hashing

--- chapter10/main.py
+++ chapter10/main.py
@@ -247,6 +247,113 @@ class Vector:
 v = Vector(range(10))
 
 print(v.z) # 2.0
-v.x = 10
 print(v.x) # 10 
 print(v) # (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+
+
+# ---------------------- Vector hashing ---------------------- #
+print('*'*60)
+print('\nVector hashing:\n')
+
+n = 0 
+for i in range(1, 6):
+    n ^= i
+
+print(n) # 1 
+
+import functools
+functools.reduce(lambda a, b: a^b, range(6))
+
+import operator
+functools.reduce(operator.xor, range(6))
+
+from array import array
+import reprlib
+import math 
+import functools
+import operator
+
+class Vector:
+    typecode = 'd'
+    shortcut_names = 'xyzt'
+    
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+        
+    def __iter__(self):
+        return iter(self._components)
+    
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return f'Vector({components})'
+    
+    def __str__(self):
+        return str(tuple(self))
+    
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+                bytes(self._components))
+    
+        
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+    
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __len__(self):
+        return len(self._components)
+    
+    def __getitem__(self, index):
+        cls = type(self)
+        if isinstance(index, slice):
+            return cls(self._components[index])
+        elif isinstance(index, numbers.Integral):
+            return self._components[index]
+        else:
+            msg = f'{cls.__name__} indices must be integers'
+            raise TypeError(msg.format(cls=cls))
+    
+    # new method to get hash
+    def __hash__(self):
+        hashes = (hash(x) for x in self._components)
+        return functools.reduce(operator.xor, hashes, 0)
+    
+    def __getattr__(self, name):
+        cls = type(self)
+        if len(name) == 1:
+            pos = cls.shortcut_names.find(name)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+            
+        msg = f'{cls.__name__!r} object has no attribute {name!r}'
+        raise AttributeError(msg)
+    
+    def __setattr__(self, name, value):
+        cls = type(self)
+        if len(name) == 1:
+            if name in cls.shortcut_names:
+                error = f'readonly attribute {name!r}'
+            elif name.islower():
+                error = f"can't set attributes 'a' to 'z' in {cls.__name__!r}"
+            else:
+                error = ''
+            if error:
+                raise AttributeError(error)
+        super().__setattr__(name, value)
+    
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
+    
+    
+v1 = Vector([1.0, 2.0, 3.0])
+v3 = Vector([4.0, 5.0, 6.0])
+print(hash(v1)) 
+print(hash(v3))
