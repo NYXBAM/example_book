44bdda93ec6c719d1f4a0811c35f4e5ba62dc1b2
NYXBAM <NYXBAM@proton.me>
2025-09-19 00:56:10 -0700

    protocols, abc interfaces

--- chapter11/protocols.py
+++ chapter11/protocols.py
@@ -29,7 +29,7 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
     
-    # this method allows item assignment
+    # this method allows item assignment (shuffling)
     def __setitem__(self, position, card):
         deck._cards[position] = card 
 
@@ -51,3 +51,40 @@ deck = FrenchDeck()
 shuffle(deck)
 print(deck[:5])
 
+# ----------------- ABCs ----------------- # 
+
+class Struggle:
+    def __len__(self): return 22
+    
+from collections import abc
+
+print(isinstance(Struggle(), abc.Sized)) # True
+
+# ---------------- Frenchdeck2.py ---------------- # 
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck2(collections.MutableSequence):
+    ranks =[str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+    
+    def __init__(self):
+        self._cards = [Card(rank, suit) 
+                       for suit in self.suits 
+                       for rank in self.ranks]
+    def __len__(self):
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+    def __setitem__(self, position, value):
+        self._cards[position] = value
+    
+    def __delitem__(self, position):
+        del self._cards[position]
+    
+    def insert(self, position, value):
+        self._cards.insert(position, value)    
