8cbd909e561356206c5187a6f832e6bcab9a285e
NYXBAM <NYXBAM@proton.me>
2025-10-07 23:53:26 -0700

    some itertools func

--- chapter14/iter.py
+++ chapter14/iter.py
@@ -300,3 +300,39 @@ def aritropgen_gen(begin, step, end=None):
     return ap_gen
 
 
+
+def vowel(c):
+    return c.lower() in "aeiou"
+
+print(list(filter(vowel, "Aardvark"))) # ['A', 'a', 'a']
+
+import itertools
+print(list(itertools.filterfalse(vowel, "Aardvark"))) # ['r', 'd', 'v', 'r', 'k']
+print(list(itertools.dropwhile(vowel, "Aardvark"))) # ['r', 'd', 'v', 'r', 'k']
+print(list(itertools.takewhile(vowel, "Aardvark"))) # ['A', 'a']
+print(list(itertools.compress("Aardvark", (1,0,1,1,0,1)))) # ['A', 'r', 'd', 'a']
+print(list(itertools.islice("Aardvark", 4))) # ['A', 'a', 'r', 'd']
+print(list(itertools.islice("Aardvark", 4, 7))) # ['v', 'a', 'r']
+print(list(itertools.islice("Aardvark", 1, 7, 2))) # ['a', 'd', 'a']
+
+
+
+sample = [5, 4, 2, 8, 7, 6, 3, 0, 9, 1]
+
+import itertools
+print(list(itertools.accumulate(sample))) # 5, 9, 11, 19, 26, 32, 35, 35, 44, 45]
+print(list(itertools.accumulate(sample, min))) # [5, 4, 2, 2, 2, 2, 2, 0, 0, 0]
+print(list(itertools.accumulate(sample, max))) # [5, 5, 5, 8, 8, 8, 8, 8, 9, 9]
+import operator
+print(list(itertools.accumulate(sample, operator.mul))) # [5, 20, 40, 320, 2240, 13440, 40320, 0, 0, 0]
+print(list(itertools.accumulate(range(1, 15), operator.mul))) # [1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200] !FACTORIAL! 
+print(list(enumerate('albatroz', 1))) # [(1, 'a'), (2, 'l'), (3, 'b'), (4, 'a'), (5, 't'), (6, 'r'), (7, 'o'), (8, 'z')]
+
+import operator
+print(list(map(operator.mul, range(11), range(11)))) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print(list(map(operator.mul, range(11), [2,4,8])))  # [0, 4, 16]
+print(list(map(lambda a, b: (a, b), range(11), [2,4,8]))) # [(0, 2), (1, 4), (2, 8)]
+print(list(itertools.starmap(operator.mul, enumerate('albatroz', 1)))) # ['a', 'll', 'bbb', 'aaaa', 'ttttt', 'rrrrrr', 'ooooooo', 'zzzzzzzz']
+sample = [5, 4, 2, 8, 7, 6, 3, 0, 9, 1]
+print(list(itertools.starmap(lambda a, b: b/a, 
+                             enumerate(itertools.accumulate(sample), 1)))) # [5.0, 4.5, 3.6666666666666665, 4.75, 5.2, 5.333333333333333, 5.0, 4.375, 4.888888888888889, 4.5]
