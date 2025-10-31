8db49e420475326b86efba5cf019c17236162bab
NYXBAM <NYXBAM@proton.me>
2025-10-31 04:41:25 -0700

    continue asyncio

--- chapter18/flags2_asyncio.py
+++ chapter18/flags2_asyncio.py
@@ -46,8 +46,49 @@ def download_one(cc, base_url, semaphore, verbose):
         status = HTTPStatus.OK
         msg = 'OK'
         
-        
     if verbose and msg:
         print(cc, msg)
         
     return Result(status, cc)
+
+@asyncio.coroutine
+def downloader_coro(cc_list, base_url, verbose, concur_req):
+    counter = collections.Counter()
+    semaphore = asyncio.Semaphore(concur_req)
+    to_do = [
+        download_one(cc, base_url, semaphore, verbose)
+        for cc in sorted(cc_list)
+    ]
+    to_do_iter = asyncio.as_completed(to_do)
+    if not verbose:
+        to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+        
+    for fututre in to_do_iter:
+        try:
+            res = yield from fututre
+        except FetchError as exc:
+            country_code = exc.country_code
+            try:
+                error_msg = exc.__cause__.__class__.args[0]
+            except IndexError:
+                error_msg = exc.__cause__.__class__.__name__
+            if verbose and error_msg:
+                msg = '*** Error for {}: {}'
+                print(msg.format(country_code, error_msg))
+            status = HTTPStatus.ERROR
+        else:
+            status = res.status
+        counter[status] += 1
+        
+    return counter
+
+def download_many(cc_list, base_url, verbose, concur_req):
+    loop = asyncio.get_event_loop()
+    coro = downloader_coro(cc_list, base_url, verbose, concur_req)
+    counts = loop.run_until_complete(coro)
+    loop.close()
+    
+    return counts
+
+if __name__ == '__main__':
+    main(download_many, DEFAULT_CONCUR_REQ, MAX_CONCUR_REQ)
