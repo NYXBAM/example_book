91c336a00f8259154e8267ce5911abdbb5e387d9
NYXBAM <delirium.dev@proton.me>
2025-11-10 03:49:19 -0800

    Descriptor Factory

--- chapter19/bulkfood_v2prop.py
+++ chapter19/bulkfood_v2prop.py
@@ -6,7 +6,7 @@ def quantity(storage_name):
         return instance.__dict__[storage_name]
     def qty_setter(instance, value):
         if value > 0:
-            instance.__dic__[storage_name] = value
+            instance.__dict__[storage_name] = value
         else: 
             raise ValueError('value must be > 0')
         
