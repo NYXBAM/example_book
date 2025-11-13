85409215caac6c5e1c2d02516dd4c24b26356dc5
NYXBAM <delirium.dev@proton.me>
2025-11-13 01:48:36 -0800

    chapter 20, props

---
+++ chapter20/bulkfood_v4prop.py
@@ -0,0 +1,19 @@
+def quantity():
+    try:
+        quantity.counter =+ 1
+    except AttributeError:
+        quantity.counter = 0
+        
+    storage_name = '_{}:{}'.format('quantity', quantity.counter)
+    
+    def qty_getter(instance):
+        return getattr(instance, storage_name)
+    
+    def qty_settter(instance, value):
+        if value > 0:
+            setattr(instance, storage_name, value)
+        else:
+            raise ValueError('Value must be > 0')
+        
+    return property(qty_getter, qty_settter)
+
