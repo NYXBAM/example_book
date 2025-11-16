117c325a20148406c9f690d8aff03bcf7457031b
NYXBAM <delirium.dev@proton.me>
2025-11-16 00:12:44 -0800

    overriding descriptors

--- chapter20/descriptorkinds.py
+++ chapter20/descriptorkinds.py
@@ -42,4 +42,12 @@ class Managed:
     def spam(self):
         print('-> Managed.spam({})'.format(display(self)))
         
-        
+    
+obj = Managed()
+obj.over # -> Overriding.__get__(<Overriding object, <Managed object, <class Managed>)
+Managed.over # -> Overriding.__get__(<Overriding object, None, <class Managed>)
+obj.over = 7 # -> Overriding.__set__(<Overriding object, <Managed object, 7)
+obj.over # -> Overriding.__get__(<Overriding object, <Managed object, <class Managed>)
+obj.__dict__['over'] = 8
+print(vars(obj)) # {'over': 8}
+obj.over # -> Overriding.__get__(<Overriding object, <Managed object, <class Managed>)
